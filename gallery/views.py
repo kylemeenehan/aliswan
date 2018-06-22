6ff8c089e4b261d327d3cde48da0d3f21d7bbecc
Kyle Meenehan <kylemeenehan@gmail.com>
2018-06-22 16:45:07 +0200

    added bulma

--- gallery/views.py
+++ gallery/views.py
@@ -10,10 +10,8 @@ def index(request):
     return render(request, 'gallery/index.html', context=params)
 
 def collection(request, collection_name):
-    print("collection_name: " + collection_name)
     collection = Collection.objects.get(name=collection_name)
     works = Work.objects.filter(collection=collection)
-    print("works: " + str(works))
     params = { 'collection': collection, 'works': works }
     return render(request, 'gallery/collection.html', params)
 
