060e7ad3fd7ed173059bc0a72df7ee80853ceeff
Kyle Meenehan <kylemeenehan@gmail.com>
2018-06-27 17:41:55 +0200

    added media url

--- aliswan/settings.py
+++ aliswan/settings.py
@@ -17,6 +17,8 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
 STATIC_DIR = os.path.join(BASE_DIR, 'static');
 STATIC_ROOT = os.path.join(BASE_DIR, 'static');
+MEDIA_URL='/'
+MEDIA_ROOT = os.path.join(STATIC_ROOT, 'static');
 
 
 # Quick-start development settings - unsuitable for production
