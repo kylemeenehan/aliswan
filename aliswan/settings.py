9e66ce13664c142d6b63f694cbcd4603f6824c10
Kyle Meenehan <kylemeenehan@gmail.com>
2018-06-27 17:03:31 +0200

    updated allowed hosts

--- aliswan/settings.py
+++ aliswan/settings.py
@@ -56,7 +56,10 @@ MIDDLEWARE = [
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
 ]
 
-CORS_ORIGIN_ALLOW_ALL = True
+CORS_ORIGIN_WHITELIST = (
+    'aliswan.co.za',
+    'localhost:4200'
+)
 
 ROOT_URLCONF = 'aliswan.urls'
 
