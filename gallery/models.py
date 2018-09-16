c9f5836c7420df93d30af1a7a4d5848107a95ade
chris <chris@topher.co.za>
2018-09-16 15:44:10 +0200

    try fix photos path2

--- gallery/models.py
+++ gallery/models.py
@@ -7,7 +7,7 @@ def image_upload_handler(self, filename):
 
 
 def photo_upload_handler(self, filename):
-    return 'static/images/%s.jpg' % (self.name)
+    return 'static/images/photograph/%s.jpg' % (self.name)
 # Create your models here.
 
 
