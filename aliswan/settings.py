52f94da22a07e527c751dbf5ac4fe403901aa652
chris <chris@topher.co.za>
2019-02-03 17:44:13 +0200

    trying 6

--- aliswan/settings.py
+++ aliswan/settings.py
@@ -15,10 +15,10 @@ import os
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
-STATIC_ROOT = os.path.join(BASE_DIR, 'var', 'static')
 STATIC_ROOT = os.path.join(BASE_DIR, 'static')
+STATIC_DIR = os.path.join(BASE_DIR, 'static')
 MEDIA_URL = '/'
-MEDIA_ROOT = os.path.join(MEDIA_URL, 'static')
+MEDIA_ROOT = os.path.join(STATIC_ROOT, 'static')
 
 
 # Quick-start development settings - unsuitable for production
