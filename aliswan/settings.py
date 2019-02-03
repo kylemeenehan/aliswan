8930293e1ac236f1d9bef8b64bf0e6612a6a088b
chris <chris@topher.co.za>
2019-02-03 17:39:01 +0200

    trying 3

--- aliswan/settings.py
+++ aliswan/settings.py
@@ -18,7 +18,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 STATIC_ROOT = os.path.join(BASE_DIR, 'var', 'static')
 STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 MEDIA_URL = '/'
-MEDIA_ROOT = os.path.join(STATIC_ROOT, 'static')
+MEDIA_ROOT = os.path.join(MEDIA_URL, 'static')
 
 
 # Quick-start development settings - unsuitable for production
